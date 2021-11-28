f93f28d11a6b15abb169a3e6b9826d2d888d70ae
ioriayaka <ioriayaka@hotmail.com>
2021-11-28 23:10:48 +0800

    add port 8080

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const routes = require('./routes')
 require('./config/mongoose')
-const port = process.env.port
+const port = process.env.port || 8080
 
 
 app.use(express.urlencoded({ extended: true }))
